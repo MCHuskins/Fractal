c8754f7ff0632f87380ed835f6d909ef1a17df55
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2020-10-16 21:15:15 -0700

    Create fractal.py

---
+++ fractal.py
@@ -0,0 +1,406 @@
+import turtle
+q=420
+#top
+a = turtle.Turtle()
+a.hideturtle()
+a.setheading(90)
+a.color('black')
+
+b = turtle.Turtle()
+b.hideturtle()
+b.setheading(90)
+b.color('black')
+
+c = turtle.Turtle()
+c.hideturtle()
+c.setheading(90)
+c.color('black')
+
+d = turtle.Turtle()
+d.hideturtle()
+d.setheading(90)
+d.color('black')
+
+e = turtle.Turtle()
+e.hideturtle()
+e.setheading(90)
+e.color('black')
+
+f = turtle.Turtle()
+f.hideturtle()
+f.setheading(90)
+f.color('black')
+
+g = turtle.Turtle()
+g.hideturtle()
+g.setheading(90)
+g.color('black')
+
+h = turtle.Turtle()
+h.hideturtle()
+h.setheading(90)
+h.color('black')
+
+#b
+z = turtle.Turtle()
+z.hideturtle()
+z.setheading(90)
+z.color('black')
+
+y = turtle.Turtle()
+y.hideturtle()
+y.setheading(90)
+y.color('black')
+
+v = turtle.Turtle()
+v.hideturtle()
+v.setheading(90)
+v.color('black')
+
+w = turtle.Turtle()
+w.hideturtle()
+w.setheading(90)
+w.color('black')
+
+u = turtle.Turtle()
+u.hideturtle()
+u.setheading(90)
+u.color('black')
+
+t = turtle.Turtle()
+t.hideturtle()
+t.setheading(90)
+t.color('black')
+
+s = turtle.Turtle()
+s.hideturtle()
+s.setheading(90)
+s.color('black')
+
+r = turtle.Turtle()
+r.hideturtle()
+r.setheading(90)
+r.color('black')
+
+def io(x):
+    a.forward(x/2)
+    a.left(90)
+    a.forward(x/3)
+    a.left(180)
+    a.forward(x/3*2)
+
+    b.forward(x/2)
+    b.left(90)
+    b.forward(x/3)
+    b.left(180)
+
+    #bottom
+    z.left(180)
+    z.forward(x/2)
+    z.left(90)
+    z.forward(x/3)
+    z.left(180)
+    z.forward(x/3*2)
+
+    y.left(180)
+    y.forward(x/2)
+    y.left(90)
+    y.forward(x/3)
+    y.left(180)
+
+    #latter secation
+    c.forward(x/2)
+    c.left(90)
+    c.forward(x/3)
+    c.left(180)
+    c.forward(x/3*2)
+
+    d.forward(x/2)
+    d.left(90)
+    d.forward(x/3)
+    d.left(180)
+
+    v.left(180)
+    v.forward(x/2)
+    v.left(90)
+    v.forward(x/3)
+    v.left(180)
+    v.forward(x/3*2)
+
+    w.left(180)
+    w.forward(x/2)
+    w.left(90)
+    w.forward(x/3)
+    w.left(180)
+
+    e.forward(x/2)
+    e.left(90)
+    e.forward(x/3)
+    e.left(180)
+    e.forward(x/3*2)
+
+    f.forward(x/2)
+    f.left(90)
+    f.forward(x/3)
+    f.left(180)
+
+    g.forward(x/2)
+    g.left(90)
+    g.forward(x/3)
+    g.left(180)
+    g.forward(x/3*2)
+
+    h.forward(x/2)
+    h.left(90)
+    h.forward(x/3)
+    h.left(180)
+
+    u.left(180)
+    u.forward(x/2)
+    u.left(90)
+    u.forward(x/3)
+    u.left(180)
+    u.forward(x/3*2)
+
+    t.left(180)
+    t.forward(x/2)
+    t.left(90)
+    t.forward(x/3)
+    t.left(180)
+
+    s.left(180)
+    s.forward(x/2)
+    s.left(90)
+    s.forward(x/3)
+    s.left(180)
+    s.forward(x/3*2)
+
+    r.left(180)
+    r.forward(x/2)
+    r.left(90)
+    r.forward(x/3)
+    r.left(180)
+
+def it(x):
+    a.left(90)
+    a.forward(x/3)
+    a.left(180)
+    a.forward(x/3*2)
+
+    b.left(90)
+    b.forward(x/3)
+    b.left(180)
+    b.forward(x/3*2)
+
+    #bottom
+    z.left(90)
+    z.forward(x/3)
+    z.left(180)
+    z.forward(x/3*2)
+
+    y.left(90)
+    y.forward(x/3)
+    y.left(180)
+    y.forward(x/3*2)
+
+    #leater
+    c.left(90)
+    c.forward(x/3)
+    d.left(90)
+    d.forward(x/3)
+
+    v.left(90)
+    v.forward(x/3)
+    w.left(90)
+    w.forward(x/3)
+
+    e.left(90)
+    e.forward(x/3)
+    e.left(180)
+    e.forward(x/3*2)
+
+    f.left(90)
+    f.forward(x/3)
+    f.left(180)
+    f.forward(x/3*2)
+
+    g.left(90)
+    g.forward(x/3)
+
+    h.left(90)
+    h.forward(x/3)
+
+    u.left(90)
+    u.forward(x/3)
+    u.left(180)
+    u.forward(x/3*2)
+
+    t.left(90)
+    t.forward(x/3)
+    t.left(180)
+    t.forward(x/3*2)
+
+    r.left(90)
+    r.forward(x/3)
+
+    s.left(90)
+    s.forward(x/3)
+
+def ittt(x):
+    a.left(90)
+    a.forward(x/3)
+    a.left(180)
+    a.forward(x/3*2)
+
+    b.left(90)
+    b.forward(x/3)
+    b.left(180)
+    b.forward(x/3*2)
+
+    c.left(90)
+    c.forward(x/3)
+    c.left(180)
+    c.forward(x/3*2)
+
+    d.left(90)
+    d.forward(x/3)
+    d.left(180)
+    d.forward(x/3*2)
+    #bottom
+    z.left(90)
+    z.forward(x/3)
+    z.left(180)
+    z.forward(x/3*2)
+
+    y.left(90)
+    y.forward(x/3)
+    y.left(180)
+    y.forward(x/3*2)
+
+    w.left(90)
+    w.forward(x/3)
+    w.left(180)
+    w.forward(x/3*2)
+
+    v.left(90)
+    v.forward(x/3)
+    v.left(180)
+    v.forward(x/3*2)
+    # latter
+    e.left(90)
+    e.forward(x/3)
+    e.left(180)
+
+    f.left(90)
+    f.forward(x/3)
+    f.left(180)
+
+    g.left(90)
+    g.forward(x/3)
+    g.left(180)
+
+    h.left(90)
+    h.forward(x/3)
+    h.left(180)
+
+    u.left(90)
+    u.forward(x/3)
+    u.left(180)
+
+    t.left(90)
+    t.forward(x/3)
+    t.left(180)
+
+    s.left(90)
+    s.forward(x/3)
+    s.left(180)
+
+    r.left(90)
+    r.forward(x/3)
+    r.left(180)
+
+def ifff(x):
+    a.left(90)
+    a.forward(x/3)
+    a.left(180)
+    a.forward(x/3*2)
+
+    b.left(90)
+    b.forward(x/3)
+    b.left(180)
+    b.forward(x/3*2)
+
+    c.left(90)
+    c.forward(x/3)
+    c.left(180)
+    c.forward(x/3*2)
+
+    d.left(90)
+    d.forward(x/3)
+    d.left(180)
+    d.forward(x/3*2)
+
+    e.left(90)
+    e.forward(x/3)
+    e.left(180)
+    e.forward(x/3*2)
+
+    f.left(90)
+    f.forward(x/3)
+    f.left(180)
+    f.forward(x/3*2)
+
+    g.left(90)
+    g.forward(x/3)
+    g.left(180)
+    g.forward(x/3*2)
+
+    h.left(90)
+    h.forward(x/3)
+    h.left(180)
+    h.forward(x/3*2)
+    #bottom
+    z.left(90)
+    z.forward(x/3)
+    z.left(180)
+    z.forward(x/3*2)
+
+    y.left(90)
+    y.forward(x/3)
+    y.left(180)
+    y.forward(x/3*2)
+
+    v.left(90)
+    v.forward(x/3)
+    v.left(180)
+    v.forward(x/3*2)
+
+    w.left(90)
+    w.forward(x/3)
+    w.left(180)
+    w.forward(x/3*2)
+
+    u.left(90)
+    u.forward(x/3)
+    u.left(180)
+    u.forward(x/3*2)
+
+    t.left(90)
+    t.forward(x/3)
+    t.left(180)
+    t.forward(x/3*2)
+
+    s.left(90)
+    s.forward(x/3)
+    s.left(180)
+    s.forward(x/3*2)
+
+    r.left(90)
+    r.forward(x/3)
+    r.left(180)
+    r.forward(x/3*2)
+io(q)
+it(q/3)
+ittt(q/3/3)
+ifff(q/3/3)
+turtle.mainloop()
